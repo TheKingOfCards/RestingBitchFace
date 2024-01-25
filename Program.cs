425cc35ad0926cf52912cade5082c1ff222ccfbc
Samuel Antonsson <samuel.antonsson@elev.ga.ntig.se>
2024-01-25 13:34:59 +0100

    First and Last

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using System.Text.Json;
+
+RestClient client = new("https://swapi.py4e.com/api/");
+
+
+string search = "2";
+Console.WriteLine("Put in a single number to find the planet you want");
+
+search = Console.ReadLine();
+Console.Clear();
+
+RestRequest request = new($"planets/{search}/");
+
+RestResponse response = client.GetAsync(request).Result;
+
+// Console.WriteLine(response.Content);
+
+File.WriteAllText("pokemon.json", response.Content);
+
+StarWarsPlanet planet = JsonSerializer.Deserialize<StarWarsPlanet>(response.Content);
+
+
+Console.WriteLine("Name: " + planet.name);
+Console.WriteLine("Population: " + planet.population);
+Console.WriteLine("Terrain: " + planet.terrain);
+Console.WriteLine("Rotation Period: " + planet.rotation_period);
+Console.WriteLine("Orbital Period: " + planet.orbital_period);
+
+
+Console.ReadLine();
---
+++ pokemon.cs
@@ -0,0 +1,8 @@
+public class StarWarsPlanet
+{
+    public string name {get; set;}
+    public string rotation_period {get; set;}
+    public string orbital_period {get; set;}
+    public string terrain {get; set;}
+    public string population {get; set;}
+}
